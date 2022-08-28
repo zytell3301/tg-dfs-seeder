8695f39f193126ed98396e49dab50eaf8e397605
Arshiya Kiani <zytell3301@gmail.com>
2022-08-28 23:01:26 +0430

    Add loadConfigs method in main for loading config values into Configs type

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+type configs struct {
+	ServiceInfo serviceInfo
+}
+
+type serviceInfo struct {
+	ServiceGroupId    string
+	ServiceInstanceID string
+}
 
 func main() {
-	fmt.Println("This is a test message :D")
+	configs := loadConfigs()
+	fmt.Println(configs)
+}
+
+func loadConfigs() *configs {
+	viper := viper.New()
+	viper.AddConfigPath("./configs")
+	viper.SetConfigName("configs")
+	viper.SetConfigType("yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic("An error encountered while reading config.yaml file. Did you removed .example suffix?")
+	}
+
+	configs := &configs{}
+
+	configs.ServiceInfo = serviceInfo{
+		ServiceGroupId:    viper.GetString("service-group-id"),
+		ServiceInstanceID: viper.GetString("service-instance-id"),
+	}
+
+	return configs
 }
